3d67a89b38bc85760b588bfff257c2d4bb4a80c1
Vasya <paindrom196@protonmail.com>
2018-11-19 11:36:20 +0300

    ДЗ к уроку 7
    К сожалению, был дефицит времени, поэтому не сумел оформить красиво, добился только работоспособности кода.

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * Geekbrains. Java, lvl 2.
+ * Домашнее задание к уроку 7.
+ *
+ * @ author Golovanov Igor
+ * @ version Nov 18, 2018
+ * Было очень мало времени, поэтому сделал "на скорую руку", но код работает
+ */
+
+package Server;
+
+public class Main {
+
+    public static void main(String[] args) {
+   new MyServer();
+
+        // write your code here
+    }
+}
